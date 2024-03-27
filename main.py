4f360d142674f0cf4f5aef7c6617c187054170b3
soeun6861 <163502846+soeun6861@users.noreply.github.com>
2024-03-27 21:18:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 n = int(input())
-
-for i in range(n, 0, -1):
-    for j in range(n, 0, -1):
-        if i < j:
-            print(" ", end = " ")
+cnt = 1
+for i in range(n):
+    for j in range(n):
+        if i <= j:
+            print(cnt, end = " ")
+            if cnt == 9:
+                cnt = 1
+            else:
+                cnt = cnt + 1
         else:
-            print(j, end = " ")
+            print(" ", end = " ")
     print()
